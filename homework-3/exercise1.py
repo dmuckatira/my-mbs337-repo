eaad8a5a42bbb3296fc4ea471f2c18f073baddb1
Dillon Muckatira <dmuckatira@tacc.utexas.edu>
2026-02-03 16:08:19 +0000

    finished exercise 1

---
+++ homework-3/exercise1.py
@@ -0,0 +1,54 @@
+import json
+from pydantic import BaseModel
+
+#Make class blueprint of protein attributes
+class ProteinEntry(BaseModel):
+   primaryAccession: str
+   organism: dict
+   proteinName: str
+   sequence: dict
+   geneName: str
+   function: str
+
+with open('hw3.json', 'r') as f:
+        prot_data = json.load(f)
+
+proteins = []
+for prot in prot_data["protein_list"]:
+        proteins.append(ProteinEntry(**prot))
+
+def find_total_mass(proteins):
+    total_mass = 0 
+    for p in proteins:
+        total_mass = total_mass + p.sequence['mass']
+    print(total_mass)
+
+
+        
+
+
+
+def find_large_proteins(proteins):
+        large_proteins = []
+        for p in proteins:
+                if p.sequence['length'] >= 1000:
+                        large_proteins.append(p.proteinName)
+        return large_proteins
+                
+
+
+
+def find_non_eukaryotes(proteins):
+    non_euk = []
+    for p in proteins:
+        if "Eukaryote" not in p.organism['lineage']:
+                non_euk.append(p.proteinName)
+        return non_euk
+
+
+return find_total_mass(proteins)
+return find_large_proteins(proteins)
+return find_non_eukaryotes(proteins)
+
+
+
