f4025d16a47cc6895eac5758c5f32f6b90e3412c
Dillon Muckatira <dmuckatira@tacc.utexas.edu>
2026-02-24 16:56:34 +0000

    random change

--- homework-3/exercise4.py
+++ homework-3/exercise4.py
@@ -1,14 +1,19 @@
 import json
 import yaml
 
+#reads uniprot list and stores the data
 with open("uniprot_protein_list.json", "r") as f:
     prot_data = json.load(f)
 
+#writes out the file in correct format
 with open("proteins.yaml", "w") as o:
     yaml.dump(
         prot_data,
         o,
-        sort_keys=False,        
+        #sorts keys 
+        sort_keys=False,    
+        #adds ---    
         explicit_start=True,    
+        #adds ...
         explicit_end=True       
     )
